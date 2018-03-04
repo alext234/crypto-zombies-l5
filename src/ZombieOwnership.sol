6c1f42db9797e6cfb9f43eb7eb785986f14e4ac3
alex <alex.tran234@gmail.com>
2018-03-05 05:30:08 +1100

    convert to using safemath

--- src/ZombieOwnership.sol
+++ src/ZombieOwnership.sol
@@ -16,8 +16,8 @@ contract ZombieOwnership is ZombieBattle, ERC721 {
 	}
 	
 	function _transfer(address _from, address _to, uint256 _tokenId) private {
-		ownerZombieCount[_to]++;
-		ownerZombieCount[_from]--;
+		ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
+		ownerZombieCount[_from] = ownerZombieCount[_from].sub(1);
 		zombieToOwner[_tokenId]=_to;
 		Transfer(_from, _to, _tokenId);
 	}
