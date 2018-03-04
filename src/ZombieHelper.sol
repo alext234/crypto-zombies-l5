b9666728e780b579b10760d7cf73279a5bcc02a0
alex <alex.tran234@gmail.com>
2018-03-05 05:49:53 +1100

    converting to safemath

--- src/ZombieHelper.sol
+++ src/ZombieHelper.sol
@@ -22,7 +22,7 @@ contract ZombieHelper is ZombieFeeding {
 	
 	function levelUp(uint _zombieId) external payable {
 		require(msg.value==levelUpFee);
-		zombies[_zombieId].level++;
+		zombies[_zombieId].level = zombies[_zombieId].level.add(1);
 	}
 
 	function changeName(uint _zombieId, string _newName) 
