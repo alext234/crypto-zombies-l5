9fb53e65c9fafa9efab4b99a6e759250baa299e1
alex <alex.tran234@gmail.com>
2018-03-04 15:39:20 +1100

    adding erc721 and ZombieOwnership

---
+++ src/ZombieOwnership.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.19;
+
+import "./ZombieAttack.sol";
+import "./erc721.sol";
+
+contract ZombieOwnership is ZombieAttack, ERC721 {
+
+}
---
+++ src/ZombieOwnership.t.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.19;
+
+import "ds-test/test.sol";
+import "./ZombieOwnership.sol";
+
+contract ZombieOwnershipTest is DSTest {
+
+}
